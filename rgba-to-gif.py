a76b47e6d43f09505d6cd5f4c321a9f6da26da0b
Ziggy Cross <ziggycross@me.com>
2023-06-07 14:13:12 -0700

    Abstracted conversion to function.

--- rgba-to-gif.py
+++ rgba-to-gif.py
@@ -4,17 +4,20 @@ from PIL import Image
 ANIM_FOLDER = "animation-frames/"
 OUTPUT_FILE = "ouptput.gif"
 
-files = os.listdir(ANIM_FOLDER)
-files.sort()
+def convert(input_folder, output_path):
+    files = os.listdir(input_folder)
+    files.sort()
 
-frames = [Image.open(os.path.join(ANIM_FOLDER,file)) for file in files]
-frames = [frame.convert("P") for frame in frames]
+    frames = [Image.open(os.path.join(input_folder,file)) for file in files]
+    frames = [frame.convert("P") for frame in frames]
 
-frames[0].save(OUTPUT_FILE,
-               format="GIF",
-               append_images=frames[1:],
-               save_all=True,
-               duration=40,
-               transparency=0,
-               disposal=2,
-               optimize=True)
+    frames[0].save(output_path,
+                format="GIF",
+                append_images=frames[1:],
+                save_all=True,
+                duration=40,
+                transparency=0,
+                disposal=2,
+                optimize=True)
+
+convert(ANIM_FOLDER, OUTPUT_FILE)
